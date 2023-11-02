36bd7b0bcea05531a0db5fc12e4179128353c947
Launo00 <s2101018@edu.bc.fi>
2023-11-02 13:33:08 +0200

    Acceleration ja deceleration

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public float currentSpeed = 0;
+    float moveX;
+    float moveY;
 
-    private Vector2 moveDirection;
+    public float acceleration = 2f;
+    public float deceleration = 2f;
 
-    void Update()
+    public Vector2 moveDirection;
+    private Vector2 lastRecorderDir;
+
+
+    public CharacterController controller;
+
+    
+    private void Update()
     {
         ProcessInputs();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Move();    
+        Move();   
     }
 
+    public void Awake()
+    {
+        
 
+    }
     void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+
 
         moveDirection = new Vector2(moveX, moveY).normalized;
-    }
 
+        
+    }
 
     void Move()
-    {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);   
+    { 
+
+        Vector3 direction = new Vector3(moveX, moveY, 0f).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            
+            currentSpeed += acceleration * Time.deltaTime;
+            if (currentSpeed > moveSpeed)
+            {
+                currentSpeed = moveSpeed;
+            }
+            controller.Move(direction * currentSpeed * Time.deltaTime);
+            lastRecorderDir = direction;
+        }
+
+        else
+        {
+            
+            currentSpeed -= deceleration * Time.deltaTime;
+            if (currentSpeed < 0)
+            {
+                currentSpeed = 0;
+            }
+
+            controller.Move(lastRecorderDir * currentSpeed * Time.deltaTime);
+        }
     }
+
 }
