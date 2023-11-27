c16c3949315f1c5829d8332e6c94df319ce4328c
MyllysMikko <115700336+MyllysMikko@users.noreply.github.com>
2023-11-27 09:26:12 +0200

    Vähän kameran liikettä

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform player;
+
     [SerializeField] Vector3 cameraOffset;
+    [SerializeField] float edgePercentage = 0.02f;
+    [SerializeField] float cameraSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +19,23 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float mouseX = Input.mousePosition.x / Screen.width;
+        float mouseY = Input.mousePosition.y / Screen.height;
+
+        float xMultiplier = Mathf.Lerp(-1f, 1f, Mathf.InverseLerp(edgePercentage, 1 - edgePercentage, mouseX));
+        float yMultiplier = Mathf.Lerp(-1, 1f, Mathf.InverseLerp(edgePercentage, 1 - edgePercentage, mouseY));
+
+        Debug.Log($"{xMultiplier}, {yMultiplier}");
+
         Vector3 cameraPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+        cameraPos.x += cameraOffset.x * xMultiplier;
+        cameraPos.y += cameraOffset.y * yMultiplier;
 
-        cameraPos += cameraOffset;
 
-        transform.position = cameraPos;
+        //transform.position = player.transform.position;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPos, cameraSpeed * Time.deltaTime);
+
     }
 }
